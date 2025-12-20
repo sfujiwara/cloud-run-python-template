aafefcbd610a0a40d556582f44c438fa6380b10c
sfujiwara <shuhei.fujiwara@gmail.com>
2025-12-21 08:09:58 +0900

    fix invoke tasks

--- tasks.py
+++ tasks.py
@@ -23,14 +23,35 @@ def docker_run(ctx):
 
 
 @invoke.task
-def deploy(ctx):
-    """Build Docker image in Cloud Build and deploy it to Cloud Run."""
+def gcloud_builds_submit(ctx):
+    """Build Docker image in Cloud Build and push it to Artifact Registry."""
     project = ctx["run"]["project"]
+    ar_location = ctx["run"]["artifact_registry"]["location"]
+    repository = ctx["run"]["artifact_registry"]["repository"]
+
+    image = f"{ar_location}-docker.pkg.dev/{project}/{repository}/cloud-run-python-template:latest"
+
+    cmd = f"gcloud builds submit --project {project} -t {image} ."
+    ctx.run(cmd)
+
+
+@invoke.task
+def gcloud_run_deploy(ctx):
+    """Deploy Docker image to Cloud Run."""
+    project = ctx["run"]["project"]
+    cloud_run_location = ctx["run"]["cloud_run"]["location"]
+    artifact_registry_location = ctx["run"]["artifact_registry"]["location"]
+    repository = ctx["run"]["artifact_registry"]["repository"]
+
+    image = f"{artifact_registry_location}-docker.pkg.dev/{project}/{repository}/cloud-run-python-template:latest"
 
     cmd_lst = [
-        "gcloud builds submit",
+        "gcloud run deploy cloud-run-python-template",
         f"--project {project}",
-        "--config cloudbuild.yaml",
+        f"--image {image}",
+        f"--region {cloud_run_location}",
+        "--platform managed",
+        "--allow-unauthenticated",
     ]
     cmd = " ".join(cmd_lst)
     ctx.run(cmd)
