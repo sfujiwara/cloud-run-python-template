7280ccbf8da20a66a5e7bab0d00de07e2c9c5c81
sfujiwara <shuhei.fujiwara@gmail.com>
2025-12-21 04:13:02 +0900

    fix invoke tasks

--- tasks.py
+++ tasks.py
@@ -3,62 +3,34 @@ import uvicorn
 
 
 @invoke.task
-def start(ctx):
+def serve(ctx):
     """Start API server."""
-    uvicorn.run(app="server.main:app", reload=True)
+    uvicorn.run(app="server.main:app", port=8080, reload=True)
 
 
 @invoke.task
 def docker_build(ctx):
-    """Build Docker image."""
-    project = ctx["run"]["project"]
-    region_ar = ctx["run"]["artifact_registry"]["region"]
-    repository = ctx["run"]["artifact_registry"]["repository"]
-    cmd = f"PROJECT={project} REPOSITORY={repository} REGION={region_ar} docker-compose build"
-    ctx.run(cmd)
-
-
-@invoke.task
-def docker_push(ctx):
-    """Push Docker image to Artifact Registry."""
-    project = ctx["run"]["project"]
-    region_ar = ctx["run"]["artifact_registry"]["region"]
-    repository = ctx["run"]["artifact_registry"]["repository"]
-    cmd = f"PROJECT={project} REPOSITORY={repository} REGION={region_ar} docker-compose push"
+    """Build Docker image in local environment."""
+    cmd = "docker build -t cloud-run-python-template ."
     ctx.run(cmd)
 
 
 @invoke.task
 def docker_run(ctx):
-    """Run Docker container."""
-    project = ctx["run"]["project"]
-    region_ar = ctx["run"]["artifact_registry"]["region"]
-    repository = ctx["run"]["artifact_registry"]["repository"]
-    cmd = f"PROJECT={project} REPOSITORY={repository} REGION={region_ar} docker-compose up"
+    """Run Docker container in local environment."""
+    cmd = "docker run --rm -p 8080:8080 cloud-run-python-template"
     ctx.run(cmd)
 
 
 @invoke.task
 def deploy(ctx):
-    """Deploy Docker image to Cloud Run."""
+    """Build Docker image in Cloud Build and deploy it to Cloud Run."""
     project = ctx["run"]["project"]
-    region_ar = ctx["run"]["artifact_registry"]["region"]
-    repository = ctx["run"]["artifact_registry"]["repository"]
-    region_cr = ctx["run"]["cloud_run"]["region"]
-
-    image = f"{region_ar}-docker.pkg.dev/{project}/{repository}/cloud-run-python-template:latest"
 
     cmd_lst = [
-        "gcloud run deploy",
-        "--project",
-        project,
-        "--platform",
-        "managed",
-        "--region",
-        region_cr,
-        "--image",
-        image,
-        "cloud-run-python-template",
+        "gcloud builds submit",
+        f"--project {project}",
+        "--config cloudbuild.yaml",
     ]
     cmd = " ".join(cmd_lst)
     ctx.run(cmd)
